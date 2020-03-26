7fa31567c7e50497d3275745407253776b81486f
Abdul Haseeb <abdulhaseeb13mar@gmail.com>
2020-03-26 07:42:52 +0500

    adding different routes

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use("/addproduct", (req, res, next) => {
-  console.log("in the addprofuct");
-  res.send("<h1>hello add product</h1>");
+  console.log("in add product");
+
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
+  );
 });
 
+app.post("/product", (req, res, next) => {
+  console.log("in product");
+  console.log(req.body);
+  res.redirect("/");
+});
 app.use("/", (req, res, next) => {
-  console.log("in the middleware");
+  console.log("in home");
   res.send("<h1>hello</h1>");
 });
 
