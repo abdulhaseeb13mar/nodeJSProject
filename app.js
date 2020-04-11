96ae4efcb9f2965ef9b794c5f92b2de6d9c48ec5
Abdul Haseeb <abdulhaseeb13mar@gmail.com>
2020-04-11 21:41:53 +0500

    initial connection with mysql database

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const errorController = require("./controllers/error");
+const db = require("./utils/database");
 
 const app = express();
 
@@ -10,6 +11,10 @@ app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+
+db.execute("SELECT * FROM products")
+  .then((result) => console.log(result[0], result[1]))
+  .catch((err) => console.log(err));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
